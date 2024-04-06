43988dd82d7e36ce4f551fa7992aeadf666097f2
403TestUserKirill <166231731+403TestUserKirill@users.noreply.github.com>
2024-04-06 17:20:02 +0300

    First commit

---
+++ Github.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main() {
+	int number;
+
+	std::cout << "Enter a number: ";
+	std::cin >> number;
+
+	int cube = number * number * number;
+
+	std::cout << "The cube of " << number << " is: " << cube << std::endl;
+
+	return 0;
+}
